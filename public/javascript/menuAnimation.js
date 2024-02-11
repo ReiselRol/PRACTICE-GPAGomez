bee9c4777988a130f9beaacd71adf1bc9b4560ff
Rios, Ramon <125033932+ReiselRol@users.noreply.github.com>
2024-02-11 11:14:57 +0100

    another Middle

--- public/javascript/menuAnimation.js
+++ public/javascript/menuAnimation.js
@@ -2,19 +2,128 @@ var HamburgerButton = document.getElementById('NavbarHambuergerTd')
 var LateralBar = document.getElementById('LateralBar')
 var AllScreens = document.getElementById('theScreen')
 
-HamburgerButton.addEventListener('click', function() {
+var CourseT = document.getElementById('CoursesT')
+var CourseL = document.getElementById('CoursesL')
+
+var PeriodT = document.getElementById('PeriodT')
+var PeriodL = document.getElementById('PeriodL')
+
+var L1 = document.getElementById('l1')
+var L2 = document.getElementById('l2')
+
+var ProfileBtn = document.getElementById('profileBtn')
+var LogoBtn = document.getElementById('logoBtn')
+
+//<a href="{{ url('/dashboard') }}">
+
+const changeLateralUI = () => {
     if (LateralBar.classList.contains('lateralBarActived')) {
         LateralBar.classList.remove('lateralBarActived');
         LateralBar.classList.add('lateralBarUnctived');
+        ProfileBtn.classList.remove('profileShow')
+        ProfileBtn.classList.add('profileUnShow')
+
     } else if (LateralBar.classList.contains('lateralBarUnctived')) {
+        ProfileBtn.classList.remove('profileShow')
+        ProfileBtn.classList.add('profileUnShow')
         LateralBar.classList.remove('lateralBarUnctived');
         LateralBar.classList.add('lateralBarActived');
     }
     if (AllScreens.classList.contains('oscureScreen')) {
+
         AllScreens.classList.remove('oscureScreen');
         AllScreens.classList.add('normalScreen');
+
     } else if (AllScreens.classList.contains('normalScreen')) {
+
         AllScreens.classList.remove('normalScreen');
         AllScreens.classList.add('oscureScreen');
+
     }
-})
+
+    if (CourseT.classList.contains('TitleUnMovedText')) {
+
+        CourseT.classList.remove('TitleUnMovedText');
+        CourseT.classList.add('TitleMovedText');
+
+        CourseL.classList.remove('TitleUnMovedLogo');
+        CourseL.classList.add('TitleMovedLogo');
+
+        PeriodT.classList.remove('TitleUnMovedText');
+        PeriodT.classList.add('TitleMovedText');
+
+        PeriodL.classList.remove('TitleUnMovedLogo');
+        PeriodL.classList.add('TitleMovedLogo');
+
+    } else if (CourseT.classList.contains('TitleMovedText')) {
+
+        CourseT.classList.remove('TitleMovedText');
+        CourseT.classList.add('TitleUnMovedText');
+
+        CourseL.classList.remove('TitleMovedLogo');
+        CourseL.classList.add('TitleUnMovedLogo');
+
+        PeriodT.classList.remove('TitleMovedText');
+        PeriodT.classList.add('TitleUnMovedText');
+
+        PeriodL.classList.remove('TitleMovedLogo');
+        PeriodL.classList.add('TitleUnMovedLogo');
+
+    }
+    if (L1.classList.contains('inv')) {
+        L1.classList.remove('inv');
+        L2.classList.remove('inv');
+    } else {
+        L1.classList.add('inv');
+        L2.classList.add('inv');
+    }
+}
+const changeProfileUI = () => {
+    if (ProfileBtn.classList.contains('profileShow')) {
+        ProfileBtn.classList.remove('profileShow')
+        ProfileBtn.classList.add('profileUnShow')
+    } else {
+        ProfileBtn.classList.add('profileShow')
+        ProfileBtn.classList.remove('profileUnShow')
+
+        if (LateralBar.classList.contains('lateralBarActived')) {
+            LateralBar.classList.remove('lateralBarActived');
+            LateralBar.classList.add('lateralBarUnctived');
+            ProfileBtn.classList.remove('profileShow')
+            ProfileBtn.classList.add('profileUnShow')
+    
+        }
+
+        if (AllScreens.classList.contains('oscureScreen')) {
+
+            AllScreens.classList.remove('oscureScreen');
+            AllScreens.classList.add('normalScreen');
+    
+        }
+
+        if (CourseT.classList.contains('TitleMovedText')) {
+
+            CourseT.classList.remove('TitleMovedText');
+            CourseT.classList.add('TitleUnMovedText');
+    
+            CourseL.classList.remove('TitleMovedLogo');
+            CourseL.classList.add('TitleUnMovedLogo');
+    
+            PeriodT.classList.remove('TitleMovedText');
+            PeriodT.classList.add('TitleUnMovedText');
+    
+            PeriodL.classList.remove('TitleMovedLogo');
+            PeriodL.classList.add('TitleUnMovedLogo');
+    
+        }
+        if (L1.classList.contains('inv') == false) {
+            L1.classList.add('inv');
+            L2.classList.add('inv');
+        }
+    }
+}
+
+HamburgerButton.addEventListener('click', function() { changeLateralUI() })
+CourseL.addEventListener('click', function() { changeLateralUI() })
+PeriodL.addEventListener('click', function() { changeLateralUI() })
+LogoBtn.addEventListener('click', function() { changeProfileUI(false) })
--- resources/views/masters/master.blade.php
+++ resources/views/masters/master.blade.php
@@ -9,6 +9,14 @@
         @yield('extraHead')
     </head>
     <body>
+        @if (Route::has('login'))
+            @auth
+            <div class="profileInfo profileUnShow" id="profileBtn">
+                <div class="profileOption"><a class="profileOptionA" href="/profile">Profile</a></div>
+                <div class="profileOption"><a class="profileOptionA" href="/dashboard">Dashboard</a></div>
+            </div>
+            @endauth
+        @endif
         <div id="Navbar" class="navbarSizeAndPosition">
             <table id="NavbarTable" class="navbarTableSize">
                 <tr id="NavbarTableTr" class="navbarTableTrSize">
@@ -27,7 +35,7 @@
                     <td id="NavbarUser" class="navbarLoginTd">
                         @if (Route::has('login'))
                             @auth
-                            <a href="{{ url('/dashboard') }}"><img src="{{ asset('imgs/LogedUserIcon.png')}}" class="LogedIconClass"/></a>
+                            <img src="{{ asset('imgs/LogedUserIcon.png')}}" class="LogedIconClass" id="logoBtn"/>
                             @else
                             <a href="{{ route('login') }}"><button class="navbarButtonLogin">Log in</button></a>
 
@@ -43,13 +51,24 @@
         @if (Route::has('login'))
             @auth
                 <div id="LateralBar" class="lateralBar lateralBarUnctived">
-                    <table class="LateralBarTable">
-                        <tr class="LateralTableTr">
-                            <td class="tdTitle">
-                                <label class="MenuTitle">Courses</label>
-                            </td>
-                        </tr>
-                    </table>
+                    <div class="MenuTitle TitleLetter TitleUnMovedText" id="CoursesT">
+                        Courses
+                    </div>
+                    <div class="MenuTitle TitleLogo TitleUnMovedLogo" id="CoursesL">
+                        <img src="{{ asset('imgs/Courses.png') }}" class="fullImage"/>
+                    </div>
+
+                    <div style="width: 99%; top: 10vh; left: 0; position: absolute; border: white 2px solid" id="l1" class="liner inv"></div>
+                    <div class="MenuTitle Subtitle1"> <a href="/course" class="lateralMiniOption">See Courses</a></div>
+                    <div class="MenuTitle Subtitle2"> <a href="/course/create" class="lateralMiniOption">Create Course</a></div>
+
+                    <div class="MenuTitle TitleLetter2 TitleUnMovedText" id="PeriodT">
+                        Period
+                    </div>
+                    <div class="MenuTitle TitleLogo2 TitleUnMovedLogo" id="PeriodL">
+                        <img src="{{ asset('imgs/Calendar.png') }}" class="fullImage"/>
+                    </div>
+                    <div style="width: 99%; top: 34vh; left: 0; position: absolute; border: white 2px solid" id="l2" class="liner inv"></div>
                 </div>
                 <div id="theScreen" class="allScreen normalScreen">
                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900" preserveAspectRatio="xMidYMax slice"><defs><linearGradient id="bg"><stop offset="0%" style="stop-color:rgba(128, 0, 202, 0.4)"></stop><stop offset="100%" style="stop-color:rgba(68, 0, 108, 0.4)"></stop></linearGradient><path id="wave" fill="url(#bg)" d="M-363.852,502.589c0,0,236.988-41.997,505.475,0 s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z" /></defs><g><use xlink:href='#wave' opacity=".3"><animateTransform attributeName="transform" attributeType="XML" type="translate" dur="10s" calcMode="spline" values="270 230; -334 180; 270 230" keyTimes="0; .5; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" /></use><use xlink:href='#wave' opacity=".6"><animateTransform attributeName="transform" attributeType="XML" type="translate" dur="8s" calcMode="spline" values="-270 230;243 220;-270 230" keyTimes="0; .6; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" /></use><use xlink:href='#wave' opacity=".9"><animateTransform attributeName="transform" attributeType="XML" type="translate" dur="6s" calcMode="spline" values="0 230;-140 200;0 230" keyTimes="0; .4; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" /></use></g></svg>
